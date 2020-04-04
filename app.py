1df39c2ddddb33f422d8d4baa49bde9e67b79723
Muoyo Okome <mokome@gmail.com>
2020-04-04 00:26:09 -0400

    Figure aesthetics

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ import matplotlib.pyplot as plt
 # Import python files we've created to help
 
 # Controls appearance of seaborn plots. Options: paper, notebook, talk, or poster
-# SEABORN_CONTEXT = 'paper' 
+SEABORN_CONTEXT = 'paper' 
 SEABORN_PALETTE = sns.color_palette("bright")
 sns.set_palette(SEABORN_PALETTE)
-# sns.set_context(SEABORN_CONTEXT)
+sns.set_style('white')
+sns.set_context(SEABORN_CONTEXT)
 
 
 # st.title('How is your state doing against the curve?')
@@ -100,7 +101,6 @@ beds_per_state['state'] = beds_per_state['state'].apply(lambda abbrev: state_dic
 df_states = pd.read_csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
 df_states['date'] = pd.to_datetime(df_states['date'])
 df_states = df_states.loc[df_states['date'] > '2020-03-04']
-df_states = df_states.drop(columns='fips')
 df_states = df_states.merge(df_population, how='left', on='state')
 df_states = df_states.merge(beds_per_state, how='left', on='state')
 df_states['cases_per_100k_people'] = df_states['cases'] / (df_states['population']/100_000)
@@ -141,21 +141,22 @@ else:
     page_title = 'COVID-19 Cases per State'
 
 
-st.sidebar.subheader('States displayed')
+st.sidebar.subheader(f'Cases per state, Page {i+1} of {num_groups}')
     
 for j, state in enumerate(zip(states_to_plot_latest['state'], states_to_plot_latest['cases'])):
     st.sidebar.text(f'{i*group_size + j + 1} - {state[0]} - {state[1]} cases')
 
 st.markdown(f'## **{page_title} as of {pd.to_datetime(most_recent_date).strftime("%b %-d, %Y")}**')
-# st.write(f'')
-    
-sns.lineplot(x=states_to_plot['date'], y=y_val, hue=states_to_plot['state'], marker='o', ci=False)
+
+
+sns.lineplot(x=states_to_plot['date'], y=y_val, hue=states_to_plot['state'], linewidth=3, marker='o', ci=False)
 plt.xticks(rotation=90);
 plt.title(chart_title)
 sns.despine()
+plt.tight_layout()
 st.pyplot()
 
 
 
 st.markdown('### **Data source & methodology: [New York Times](https://www.nytimes.com/article/coronavirus-county-data-us.html)**')
-st.dataframe(df_states_latest)
+st.dataframe(df_states_latest.drop(columns=['date', 'fips']))
